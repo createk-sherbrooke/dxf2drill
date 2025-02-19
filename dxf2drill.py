d817c05e8e9ca3750f9af6cb63f4f0978c757cfa
Francis Thérien <frtherien@gmail.com>
2025-02-19 14:23:43 -0500

    Add dxf2drill and uv project files

--- dxf2drill.py
+++ dxf2drill.py
@@ -32,7 +32,7 @@ def get_circles(dxf: Drawing) -> list[Circle]:
     for c in msp.query("CIRCLE"):
         result.append(
             Circle(
-                center=Vec2(x=c.dxf.center.x, y=c.dxf.center.y), dia=c.dxf.radius * 2
+                center=Vec2(x=c.dxf.center.x, y=c.dxf.center.y), dia=round(c.dxf.radius * 2, 4)
             )
         )
     return result
@@ -57,28 +57,47 @@ def get_args():
     return parser.parse_args()
 
 
-def generate_xnc_header(units: str, tool_map: dict[float, int]) -> Generator[str]:
+def fmt_number(x: float, n=3):
+    """Format float with fixed precision and stripped trailing zeros"""
+    fmt = f'{{:.{n}f}}'
+    return fmt.format(x).rstrip('0')
+
+
+def generate_xnc_header(units: str, tool_map: dict[float, int]) -> Generator[str, None, None]:
     """
     units: METRIC or INCH (according to XNC spec)
     tool_map: dict of {diameter: tool_index}
     """
-    yield "M48"
+    yield "M48" # Begin header
     yield units
+
+    # Tool declarations
     for dia, idx in tool_map.items():
         yield f"T{idx:02d}C{dia:.3f}"
-    yield "%"
+    yield "%" # End of header
 
 
-def generate_xnc(circles: list[Circle]) -> Generator[str]:
+def generate_xnc(circles: list[Circle]) -> Generator[str, None, None]:
     dia_groups = group_circles_by_dia(circles)
     dia_list = list(dia_groups.keys())
     tool_map = {dia_list[i]: i for i in range(len(dia_list))}
     for cmd in generate_xnc_header("METRIC", tool_map):
         yield cmd
+    
+    # Drill mode
+    yield "G05"
+    for tool_dia, tool_idx in tool_map.items():
+        # Tool declaration
+        yield f"T{tool_idx:02d}"
+        # Drill hits for tool
+        for circ in dia_groups[tool_dia]:
+            yield f"X{fmt_number(circ.center.x)}Y{fmt_number(circ.center.y)}"
+
+    yield "M30" # End of file
 
 
 def main():
-    args = get_args
+    args = get_args()
     dxf = ezdxf.readfile(args.filename)
     circles = get_circles(dxf)
 
